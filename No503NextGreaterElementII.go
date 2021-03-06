e2c4d6d6b8af6695c9b75950a494baac702d4b10
huhongjian <294116824@qq.com>
2021-03-07 00:26:02 +0800

    503. 下一个更大元素 II

--- No503NextGreaterElementII.go
+++ No503NextGreaterElementII.go
@@ -4,8 +4,10 @@ import "fmt"
 
 /**
 503. 下一个更大元素 II
+
+其实是一个单调栈的题目
 */
-func nextGreaterElements(nums []int) []int {
+func nextGreaterElements1(nums []int) []int {
 	var len = len(nums)
 	var res []int
 	for i := 0; i < len; i++ {
@@ -32,6 +34,26 @@ func nextGreaterElements(nums []int) []int {
 		j = i + 1
 	}
 	return res
+}
+
+/**
+单调栈解法
+*/
+func nextGreaterElements(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n)
+	for i := range ans {
+		ans[i] = -1
+	}
+	stack := []int{}
+	for i := 0; i < n*2-1; i++ {
+		for len(stack) > 0 && nums[stack[len(stack)-1]] < nums[i%n] {
+			ans[stack[len(stack)-1]] = nums[i%n]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i%n)
+	}
+	return ans
 }
 
 func main() { // 声明 main 主函数
